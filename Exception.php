39f929cfff9d57529aeaba28f91e54bdfd165b2e
Sokolov Innokenty <r2.kenny@gmail.com>
2011-03-24 00:46:06 +0300

    Fix Exception

--- Exception.php
+++ Exception.php
@@ -5,6 +5,6 @@
  * @author Sokolov Innokenty, <sokolov.innokenty@gmail.com>
  * @copyright Copyright (c) 2011, qbbr
  */
-class Q_Cache_Exception extends Q_Exception
+class Q_Cache_Exception extends Exception
 {
 }
