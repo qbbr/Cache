dca2728629ae1d10902c2f57537dea04c1b1d475
Sokolov Innokenty <r2.kenny@gmail.com>
2011-05-29 17:04:15 +0400

    updt file adapter

--- Adapter/File.php
+++ Adapter/File.php
@@ -7,35 +7,47 @@
  */
 class Q_Cache_Adapter_File extends Q_Cache_Adapter_Abstract
 {
-    protected $_file;
-    
+    const EXTENSION = '.cache';
+    const SEPARATOR = '_';
+
+    protected $_cacheDir;
+
     /**
      * @param string $file Путь до файла
      * @throws Q_Logger_Exception
      */
-    public function __construct($file)
+    public function __construct($cacheDir)
     {
-        if (!is_file($file)) {
-            if (@!touch($file)) {
-                throw new Q_Cache_Adapter_Exception("File ({$file}) not found");
+        if (!is_dir($cacheDir)) {
+            if (!mkdir($cacheDir, 0777)) {
+                throw new Q_Cache_Adapter_Exception("Dir ({$cacheDir}) not found");
             }
         }
 
-        if (!is_writable($file)) {
-            throw new Q_Cache_Adapter_Exception("File ({$file}) is not writable");
+        if (!is_writable($this->_cacheDir)) {
+            throw new Q_Cache_Adapter_Exception("Dir ({$this->_cacheDir}) is not writable");
         }
+
+        $this->_cacheDir = $cacheDir;
     }
 
     public function get($key, $defaultValue = null)
     {
+        $filePath = $this->getFilePath($key);
+
+        if (!file_exists($filePath)) {
+            return $defaultValue;
+        }
     }
 
     public function has($key)
     {
+        return file_exists($this->getFilePath($key));
     }
 
     public function remove($key)
     {
+        return unlink($this->getFilePath($key));
     }
 
     public function set($key, $value, $lifetime = null)
@@ -44,5 +56,27 @@ class Q_Cache_Adapter_File extends Q_Cache_Adapter_Abstract
 
     public function flush()
     {
+        return $this->recursiveRemove($this->_cacheDir());
+    }
+
+    protected function getFilePath($key)
+    {
+        return $this->_cacheDir . DS . str_replace(self::SEPARATOR, DS, $key) . self::EXTENSION;
+    }
+
+    protected function recursiveRemove($dir)
+    {
+        $result = true;
+
+        foreach (glob($dir . DS . '*') as $file) {
+            if (is_dir($file)) {
+                $result = $this->recursiveRemove();
+                $result = rmdir($file) && $result;
+            } else {
+                $result = unlink($file) && $result;
+            }
+        }
+
+        return $result;
     }
 }
